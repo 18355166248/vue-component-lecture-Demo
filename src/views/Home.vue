de03ae6109527bb32efaf52dc98ca9323301fed2
jianglang <643546122@qq.com>
2020-02-25 15:31:03 +0800

    docs: 清楚不必要的文件代码

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,8 +1,5 @@
 <template>
   <div class="home">
-    <HelloWorld msg="Welcome to Your Vue.js App" />
-    <div>年龄: {{ age }}</div>
-    <button @click="helloMethod">触发HelloWorld组件事件</button>
     <div class="grid">
       <div v-for="item in list" :key="item">{{ item }}</div>
     </div>
@@ -10,7 +7,6 @@
 </template>
 
 <script>
-import HelloWorld from '@/components/HelloWorld.vue'
 import commonMixin from 'mixins/common'
 
 export default {
@@ -18,25 +14,11 @@ export default {
   mixins: [commonMixin],
   data() {
     return {
-      age: 0,
       list: [1, 2, 3, 4, 5, 6, 7, 8, 9],
     }
   },
-  created() {
-    this.$on('onChangeAge', this.onChangeAge)
-  },
-  methods: {
-    helloMethod() {
-      this.broadcast('B', 'changeName', { name: '改变了名字' })
-    },
-
-    onChangeAge({ age }) {
-      this.age = age
-    },
-  },
-  components: {
-    HelloWorld,
-  },
+  created() {},
+  methods: {},
 }
 </script>
 
